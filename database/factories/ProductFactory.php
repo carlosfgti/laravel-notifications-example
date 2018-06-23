1fab2854a8bca917b8ef3aa591b36b12062835c5
Carlos Ferreira <conta@especializati.com.br>
2018-06-23 14:55:36 -0300

    Define factory product

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Model::class, function (Faker $faker) {
     return [
-        //
+        'name'          => $faker->unique()->word,
+        'description'   => $faker->sentence(),
     ];
 });
