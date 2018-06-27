41d134a30c537569cc5217f6bfb2319a4e1528ab
Carlos Ferreira <conta@especializati.com.br>
2018-06-27 14:23:51 -0300

    Structure Template View

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Laravel Notifications</title>
+
+        <link rel="stylesheet" href="{{ url('css/app.css') }}">
+    </head>
+    <body>
+        @yield('content')
+        
+
+        <script src="{{ url('js/app.js') }}"></script>
+    </body>
+</html>
