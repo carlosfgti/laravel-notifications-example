c1409112e66f6eff496751a03d7a4f10e204dae5
Carlos Ferreira <conta@especializati.com.br>
2018-06-27 15:12:01 -0300

    Define user id

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -1,9 +1,11 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Models\Product;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Product::class, function (Faker $faker) {
     return [
+        'user_id'       => 1,
         'name'          => $faker->unique()->word,
         'description'   => $faker->sentence(),
     ];
