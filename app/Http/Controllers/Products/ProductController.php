445c1ec6da0198c3e3f22efd90b36f4e81d0a9dc
Carlos Ferreira <conta@especializati.com.br>
2018-06-27 14:19:45 -0300

    Constructor Product

--- app/Http/Controllers/Products/ProductController.php
+++ app/Http/Controllers/Products/ProductController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers\Products;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class ProductController extends Controller
 {
-    //
+    private $product;
+    
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
 }
