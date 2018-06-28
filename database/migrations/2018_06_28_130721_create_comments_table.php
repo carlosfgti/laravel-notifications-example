5b762669cc2d427260a9f5c0ea9c24fbd4bc81a6
Carlos Ferreira <conta@especializati.com.br>
2018-06-28 13:10:21 -0300

    Define structure table comments

--- database/migrations/2018_06_28_130721_create_comments_table.php
+++ database/migrations/2018_06_28_130721_create_comments_table.php
@@ -15,7 +15,14 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('title');
+            $table->text('body');
             $table->timestamps();
+
+            $table->foreign('user_id')
+                        ->references('id')->on('users')
+                        ->onDelete('cascade');
         });
     }
 
