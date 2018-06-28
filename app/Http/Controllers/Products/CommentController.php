44b1681295d543c5060f4a50bca20e40b38fd0f1
Carlos Ferreira <conta@especializati.com.br>
2018-06-28 13:15:50 -0300

    Store Comment Product

--- app/Http/Controllers/Products/CommentController.php
+++ app/Http/Controllers/Products/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Products;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class CommentController extends Controller
 {
@@ -10,6 +11,10 @@ class CommentController extends Controller
 
     public function store(Request $request)
     {
-        
+        $product = Product::find($request->productId);
+
+        $comment = $product->comments()->create($request->all());
+
+        return redirect()->route('products.show', $product->id);
     }
 }
