05025bfe6f04a70759ea32cb0a07d7c5fa15b57e
Carlos Ferreira <conta@especializati.com.br>
2018-06-28 13:07:08 -0300

    Create Comment Controller

---
+++ app/Http/Controllers/Products/CommentController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Products;
+
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    
+
+    public function store(Request $request)
+    {
+        
+    }
+}
