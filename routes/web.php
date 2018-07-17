7cc9cfc4d3c8310bc34d1b454309633a65bce7c5
Carlos Ferreira <conta@especializati.com.br>
2018-07-17 11:07:15 -0300

    Create View Home Page

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,6 @@ $this->post('commet', 'Products\CommentController@store')->name('comment.store')
 $this->get('product/{id}', 'Products\ProductController@show')->name('products.show');
 $this->get('products', 'Products\ProductController@index')->name('products.index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'SiteController@index');
 
 Auth::routes();
