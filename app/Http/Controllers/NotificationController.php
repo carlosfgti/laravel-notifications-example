e3f02fbf50f64fb0e748cde7c4a57c2b7574c9f6
Carlos Ferreira <conta@especializati.com.br>
2018-07-17 20:16:06 -0300

    Get Only Unread Notifications

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -8,7 +8,8 @@ class NotificationController extends Controller
 {
     public function notifications(Request $request)
     {
-        $notifications = $request->user()->notifications;
+        // $notifications = $request->user()->notifications;
+        $notifications = $request->user()->unreadNotifications;
 
         return response()->json(compact('notifications'));
     }
