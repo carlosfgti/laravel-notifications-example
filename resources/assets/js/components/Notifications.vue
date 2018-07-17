f63d3e6ea4c7305df378de5c2c247ae7b1d36049
Carlos Ferreira <conta@especializati.com.br>
2018-07-17 15:01:17 -0300

    Create Component Notifications Vue.js

---
+++ resources/assets/js/components/Notifications.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <li class="nav-item dropdown">
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                Notificações <span class="caret"></span>
+            </a>
+
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">
+                    Sem Notificações
+                </a>
+            </div>
+        </li>
+    </div>
+</template>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,17 +53,8 @@
                                 <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    Notificações <span class="caret"></span>
-                                </a>
+                            <notifications></notifications>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="#">
-                                        Sem Notificações
-                                    </a>
-                                </div>
-                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
