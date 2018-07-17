718dcd63957101c28a01dc3e6a482fb60ad81141
Carlos Ferreira <conta@especializati.com.br>
2018-07-17 11:20:12 -0300

    Pagination Products

--- app/Http/Controllers/Products/ProductController.php
+++ app/Http/Controllers/Products/ProductController.php
@@ -18,7 +18,7 @@ class ProductController extends Controller
 
     public function index()
     {
-        $products = $this->product->all();
+        $products = $this->product->paginate();
 
         return view('products.index', compact('products'));
     }
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,10 +4,14 @@
 
 <h1>Produtos</h1>
 
+<div class="row">
 @foreach ($products as $product)
-    {{ $product->name }} - {{ $product->description }} |
-    <a href="{{ route('products.show', $product->id) }}">Detalhes</a>
-    <hr>
+    <div class="col-4 text-center">
+        {{ $product->name }} |
+        <a href="{{ route('products.show', $product->id) }}">Detalhes</a>
+    </div>
 @endforeach
+</div>
+{!! $products->links() !!}
 
 @endsection
