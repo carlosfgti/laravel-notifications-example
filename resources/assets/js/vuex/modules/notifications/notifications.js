9b3fe816381ced70e027520adafe517b463ed414
Carlos Ferreira <conta@especializati.com.br>
2018-07-18 21:34:54 -0300

    Remove prefix api

--- resources/assets/js/vuex/modules/notifications/notifications.js
+++ resources/assets/js/vuex/modules/notifications/notifications.js
@@ -11,16 +11,16 @@ export default {
 
     actions: {
         getNotifications (context) {
-            return axios.get('/api/notifications')
+            return axios.get('/notifications')
                             .then(response => context.commit('LOAD_NOTIFICATIONS', response.data.notifications))
         },
 
         markAsRead (context, params) {
-            return axios.put('/api/notification-read', params)
+            return axios.put('/notification-read', params)
         },
 
         markAllAsRead (context, params) {
-            return axios.put('/api/notification-all-read')
+            return axios.put('/notification-all-read')
         },
     },
 
