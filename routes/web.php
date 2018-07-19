c2594d636d989f6d2040894a28fbf338d5fa12c7
Carlos Ferreira <conta@especializati.com.br>
2018-07-18 21:34:01 -0300

    Define routes notifications

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
+/**
+ * Routes notifications
+ */
+$this->put('notification-all-read', 'NotificationController@markAllAsRead')->name('notification.read.all');
+$this->put('notification-read', 'NotificationController@markAsRead')->name('notification.read');
+$this->get('notifications', 'NotificationController@notifications')->name('notifications');
+
 
 $this->post('commet', 'Products\CommentController@store')->name('comment.store');
-
 $this->get('product/{id}', 'Products\ProductController@show')->name('products.show');
 $this->get('products', 'Products\ProductController@index')->name('products.index');
 
