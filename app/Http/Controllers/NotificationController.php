c0b25ac104c7b147c9e11695e1283757b81c8e94
Carlos Ferreira <conta@especializati.com.br>
2018-07-23 12:02:38 -0300

    Middleware auth NotificationController

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 class NotificationController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
     public function notifications(Request $request)
     {
         // $notifications = $request->user()->notifications;
