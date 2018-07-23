5ebc82e3792b1af4b20b6577d6f41e5f3b435261
Carlos Ferreira <conta@especializati.com.br>
2018-07-23 13:39:20 -0300

    Iprove layout

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,20 +4,22 @@
 
 <h1>Produtos</h1>
 
-<div class="row">
+<div class="row text-center">
 @foreach ($products as $product)
-    @if (auth()->check() && auth()->user()->id == $product->user_id)
-    <div class="col-4 owner card text-center">
-    @else
-    <div class="col-4 card text-center">
-    @endif
-        <div class="card-body">
-                <div class="card-title">
-                    {{ $product->name }}
-                </div>
-                <a href="{{ route('products.show', $product->id) }}" class="btn btn-primary">
-                    Detalhes
-                </a>
+    <div class="col-4">
+        @if (auth()->check() && auth()->user()->id == $product->user_id)
+        <div class="card owner">
+        @else
+        <div class="card">
+        @endif
+            <div class="card-body">
+                    <div class="card-title">
+                        {{ $product->name }}
+                    </div>
+                    <a href="{{ route('products.show', $product->id) }}" class="btn btn-primary">
+                        Detalhes
+                    </a>
+            </div>
         </div>
     </div>
 @endforeach
