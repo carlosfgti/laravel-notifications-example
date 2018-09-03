ece31b7930d7d9d9dfcad5359a9b5552695a6187
Carlos Ferreira <conta@especializati.com.br>
2018-09-03 16:39:24 -0300

    Real-time Notifications Listeners

--- resources/assets/js/Echo.js
+++ resources/assets/js/Echo.js
@@ -1,20 +1,17 @@
 
-if (Laravel.user) {
-
-    console.log(`App.User.${Laravel.user}`)
-
-    Echo.private(`App.User.${Laravel.user}`)
-        .notification((notification) => {
-            console.log(notification);
-        });
-    Echo.private(`App.User.${Laravel.user}`)
-        .listen('PostCommented', (e) => {
-            console.log(e);
-        });
-
+let types = {
+    postCommented: 'App\Notifications\PostCommented',
 }
 
-Echo.private(`teste`)
+if (Laravel.user) {
+
+    console.log(`App.Models.User.${Laravel.user}`)
+
+    window.Echo.private(`App.Models.User.${Laravel.user}`)
         .notification((notification) => {
-            console.log(notification);
-        });
+            if (notification.type == types.postCommented) {
+                console.log(notification)
+            }
+        })
+
+}
